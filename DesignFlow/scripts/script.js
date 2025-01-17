c86ec9de9d642f232174aff4a4f09bb58711ff00
Bhagyabaragi <bhagyabaragi@gmail.com>
2025-01-17 13:32:55 +0530

    Add files via upload

---
+++ DesignFlow/scripts/script.js
@@ -0,0 +1,5 @@
+document.getElementById("designRequestForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Design request submitted successfully!");
+  });
+  
